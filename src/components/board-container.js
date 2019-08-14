13d26df5598f333687844519389dd85f81b8be43
AntPzk90 <44503472+AntPzk90@users.noreply.github.com>
2019-08-14 11:19:07 +0200

    add hone work 2, add webpack congfig in project, fix eslint bugs, crushed on components

---
+++ src/components/board-container.js
@@ -0,0 +1,5 @@
+// ф-ция возвращает главный контейнер плриложения ".board container"
+export const markupMainBoardContainer = () => {
+  return `<section class="board container">
+  </section>`;
+};
---
+++ src/components/load-more-btn.js
@@ -0,0 +1,4 @@
+// ф-ция создвния кнопки "load moreconst markupLoadMore = () => {
+export const markupLoadMore = () => {
+  return `<button class="load-more" type="button">load more</button>`;
+};
