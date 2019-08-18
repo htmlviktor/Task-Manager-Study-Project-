7583449e36f8131e900b2a00ac84d8cd968d15cc
AntPzk90 <44503472+AntPzk90@users.noreply.github.com>
2019-08-18 13:28:04 +0200

    add load more button functional

--- src/main.js
+++ src/main.js
@@ -12,7 +12,6 @@ import {filterData} from './components/data.js';
 
 const mainControl = document.querySelector(`.main__control`);
 const main = document.querySelector(`.main`);
-
 const createTasksArray = (count) => {
   let tasksArray = [];
   for (let i = 0; i < count; i++) {
@@ -22,6 +21,7 @@ const createTasksArray = (count) => {
 };
 const allTasks = createTasksArray(21);
 const dataFilter = filterData(allTasks);
+
 // ф-ция для вставки елемента на страницу
 const addMarkupElement = (container, markup) => {
   container.insertAdjacentHTML(`beforeend`, markup);
@@ -46,10 +46,31 @@ addMarkupInMainBoardContainer(markupBoardTasks());
 addMarkupCard(markupCardEdit());
 addMarkupInMainBoardContainer(markupLoadMore());
 
-const addTasks = (tasks) => {
-  for (let task of tasks) {
-    addMarkupCard(markupCrad(task.description, task.dueDate, task.tags, task.color, task.repeatingDays, task.isFavorite, task.isActive));
+const addTasks = (task) => {
+  for (let i = 0; i < 7; i++) {
+    addMarkupCard(markupCrad(task[i].description, task[i].dueDate, task[i].tags, task[i].color, task[i].repeatingDays, task[i].isFavorite, task[i].isActive));
   }
 };
-
 addTasks(allTasks);
+
+const loadMorebBTN = document.querySelector(`.load-more`);
+
+loadMorebBTN.addEventListener(`click`, (evt) => {
+  evt.preventDefault();
+  let tasks = allTasks;
+  let cardsIndom = document.querySelectorAll(`.card`);
+  let countCard = cardsIndom.length;
+  if (countCard % 8 === 0) {
+    countCard += 8;
+  }
+  if (countCard >= tasks.length) {
+    countCard = tasks.length;
+    loadMorebBTN.style.display = `none`;
+  }
+  for (let card of cardsIndom) {
+    card.remove();
+  }
+  for (let i = 0; i < countCard; i++) {
+    addMarkupCard(markupCrad(tasks[i].description, tasks[i].dueDate, tasks[i].tags, tasks[i].color, tasks[i].repeatingDays, tasks[i].isFavorite, tasks[i].isActive));
+  }
+});
